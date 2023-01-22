45b20ace9716cb27b6a537fbb184fd769bed5465
Alexander Waya Nguku <alex.waya@gmail.com>
2023-01-22 12:34:59 +0300

    file path declaration for dot env on production

--- user_management/settings.py
+++ user_management/settings.py
@@ -1,8 +1,13 @@
 import os
 from pathlib import Path
 
+# from dotenv import load_dotenv
+# load_dotenv()
+
 from dotenv import load_dotenv
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
