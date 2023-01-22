52bbca1386866152abc738d32a515a35a70f0c81
Alexander Waya Nguku <alex.waya@gmail.com>
2023-01-22 11:24:10 +0300

    add registration routing

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', home, name='users-home'),
+    path('register/', RegisterView.as_view(), name='users-register'),
 ]
