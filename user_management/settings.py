8b29385dc32eaa4adb3211248aa5ec4d48360db6
Alexander Waya Nguku <alex.waya@gmail.com>
2023-01-22 12:30:37 +0300

    initiate dot-env

--- user_management/settings.py
+++ user_management/settings.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zak1mqgp9&^ftws)2m6t(&jc3!#0wmcf6s=1550&i%czarm9*-'
+#SECRET_KEY = 'django-insecure-zak1mqgp9&^ftws)2m6t(&jc3!#0wmcf6s=1550&i%czarm9*-'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
