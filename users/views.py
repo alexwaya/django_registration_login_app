823a6eb0153d7fc5eb2bd17909dca45163b7f064
Alexander Waya Nguku <alex.waya@gmail.com>
2023-01-22 12:07:52 +0300

    login custom view

--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views import View
 
+from django.contrib.auth.views import LoginView
+from .forms import RegisterForm, LoginForm
+
 from .forms import RegisterForm
 
 # Create your views here.
@@ -31,3 +34,21 @@ class RegisterView(View):
             return redirect(to='/')
 
         return render(request, self.template_name, {'form': form})
+
+
+
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+
+    def form_valid(self, form):
+        remember_me = form.cleaned_data.get('remember_me')
+
+        if not remember_me:
+            # set session expiry to 0 seconds. So it will automatically close the session after the browser is closed.
+            self.request.session.set_expiry(0)
+
+            # Set session as modified to force data updates/cookie to be saved.
+            self.request.session.modified = True
+
+        # else browser session will be as long as the session cookie time "SESSION_COOKIE_AGE" defined in settings.py
+        return super(CustomLoginView, self).form_valid(form)
