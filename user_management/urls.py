9e843c99978c9992439cee830056d961709a1b97
Alexander Waya Nguku <alex.waya@gmail.com>
2023-01-22 14:36:20 +0300

    include the social auth app's urls

--- user_management/urls.py
+++ user_management/urls.py
@@ -16,12 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+#from django.conf.urls import url
+from django.urls import re_path
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
+
+    #url(r'^oauth/', include('social_django.urls', namespace='social')),
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),
+
 ]
 
 if settings.DEBUG:
