982ca83c06e786aced7468634c83b500991cbaf1
Alexander Waya Nguku <alex.waya@gmail.com>
2023-01-22 17:08:59 +0300

    add password reset logic

--- user_management/urls.py
+++ user_management/urls.py
@@ -23,6 +23,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from users.views import ResetPasswordView
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,6 +34,14 @@ urlpatterns = [
     re_path(r'^oauth/', include('social_django.urls', namespace='social')),
 
     path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/passwords/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/passwords/password_reset_complete.html'),
+         name='password_reset_complete'),
 
 ]
 
